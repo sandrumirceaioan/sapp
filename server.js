0e77f72fc7c0b6ade4cee7f4019fff377253d684
sandrumirceaioan <sandrumirceaioan@gmail.com>
2021-11-02 23:25:23 +0200

    heroku worst trash ever

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-// express server
-const express = require('express');
-const path = require('path');
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(requireHTTPS);
+app.use(express.static('./dist/secret-app'));
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/secret-app/index.html'));
-  });
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/secret-app/'}),
+);
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
